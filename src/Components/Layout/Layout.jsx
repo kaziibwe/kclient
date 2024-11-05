86e24cd1c5b2f1dc761fb64653fad2ca73498c09
kaziibwe <alfredkaziibwe19@gmial.com>
2024-11-05 15:45:33 +0300

    change in the login code ,toggleing dashboard through layout.js

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -18,11 +18,15 @@ const IMAGE_URL =import.meta.env.KCLIENT_IMAGE_URL;
   // const [isSidebarVisible, setIsSidebarVisible] = useState(true);
 
   // Function to toggle the sidebar
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+  // const [isProfileDropdownVisible, setIsProfileDropdownVisible] = useState(false);
+  
   const toggleSidebar = () => {
     setIsSidebarVisible(!isSidebarVisible);
   };
 
-  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+
   const [dropdowns, setDropdowns] = useState({
     products: false,
     orders: false,
@@ -31,11 +35,15 @@ const IMAGE_URL =import.meta.env.KCLIENT_IMAGE_URL;
     manage: false,
   });
 
-  const toggleDropdown = (dropdownName) => {
-    setDropdowns((prev) => ({
-      ...prev,
-      [dropdownName]: !prev[dropdownName],
-    }));
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const toggleDropdown = (e) => {
+    e.preventDefault();
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+
+  const toggleProfileDropdown = () => {
+    setIsProfileDropdownVisible(!isProfileDropdownVisible);
   };
 
   return (
@@ -210,62 +218,60 @@ const IMAGE_URL =import.meta.env.KCLIENT_IMAGE_URL;
 
         </li>
 
-        <li class="nav-item dropdown pe-3">
+        <li className="nav-item dropdown pe-3">
+      <a 
+        className="nav-link nav-profile d-flex align-items-center pe-0"
+        href="#" 
+        onClick={toggleDropdown}
+      >
+        <img src={`${IMAGE_URL}/${user?.image}`} alt="Profile" className="rounded-circle" />
+        <span className="d-none d-md-block dropdown-toggle ps-2">
+          {user ? user.name : null}
+        </span>
+      </a>
 
-          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src={`${IMAGE_URL}/${user.image}`} alt="Profile" class="rounded-circle"/>
-            
-            <span class="d-none d-md-block dropdown-toggle ps-2">{user ?user.name:null}</span>
-          </a>
+      {isDropdownVisible && (
+        <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile show">
+          <li className="dropdown-header">
+            <h6>{user ? user.name : null}</h6>
+            <span>{user ? user.name : null}</span>
+          </li>
+          <li><hr className="dropdown-divider" /></li>
 
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-            <li class="dropdown-header">
-              <h6>{user ?user.name:null}</h6>
-              <span>{user ?user.name:null}</span>
-            </li>
-            <li>
-              <hr class="dropdown-divider"/>
-            </li>
+          <li>
+            <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <i className="bi bi-person"></i>
+              <span>My Profile</span>
+            </a>
+          </li>
+          <li><hr className="dropdown-divider" /></li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-                <i class="bi bi-person"></i>
-                <span>My Profile</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider"/>
-            </li>
+          <li>
+            <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <i className="bi bi-gear"></i>
+              <span>Account Settings</span>
+            </a>
+          </li>
+          <li><hr className="dropdown-divider" /></li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-                <i class="bi bi-gear"></i>
-                <span>Account Settings</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider"/>
-            </li>
+          <li>
+            <a className="dropdown-item d-flex align-items-center" href="pages-faq.html">
+              <i className="bi bi-question-circle"></i>
+              <span>Need Help?</span>
+            </a>
+          </li>
+          <li><hr className="dropdown-divider" /></li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-                <i class="bi bi-question-circle"></i>
-                <span>Need Help?</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider"/>
-            </li>
+          <li>
+            <Link className="dropdown-item d-flex align-items-center" to="/login">
+              <i className="bi bi-box-arrow-right"></i>
+              <span>Sign Out</span>
+            </Link>
+          </li>
+        </ul>
+      )}
+    </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-              </a>
-            </li>
-
-          </ul>
-        </li>
 
       </ul>
     </nav>
